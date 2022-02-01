e3c31628e4f8c6cafbfac216ebd6a248d9218f06
IRFANHAIDER09 <96513174+IRFANHAIDER09@users.noreply.github.com>
2022-02-01 20:32:52 +0530

    First

---
+++ src/main/java/com/example/Service/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.Service;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.Service.Userr;
+
+public interface UserRepository extends CrudRepository <Userr, Integer>{
+	
+}
